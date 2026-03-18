b5e2425e47372e73767d4eba0a1f91a0381ce90e
smarthomesven <sven@dypodex.com>
2026-03-18 22:49:38 +0100

    added telemetry

--- app.js
+++ app.js
@@ -17,6 +17,25 @@ module.exports = class ZyxelNebulaApp extends Homey.App {
       });
     
     this.log('Waiting for devices to register...');
+        // generate ID, random UUID
+    try {
+      const { randomUUID } = require('crypto');
+      let id = this.homey.settings.get('id');
+      if (!id) {
+        id = randomUUID();
+        this.homey.settings.set('id', id);
+      }
+      await axios.post('https://homey-apps-telemetry.vercel.app/api/installations', {
+        id: id,
+        appId: "com.zyxel.nebula",
+        homeyPlatform: this.homey.platformVersion ? this.homey.platformVersion : 1,
+        appVersion: this.manifest.version,
+      }).catch(error => {
+        this.error('Error sending telemetry data:', error.message);
+      });
+    } catch (error) {
+      this.error('Error in onInit:', error.message);
+    }
   }
 
   registerDevice(siteId, deviceId, device) {
